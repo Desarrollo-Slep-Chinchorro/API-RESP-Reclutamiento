b5a2911a527109df68f5c5bb08901287b9a8a953
Michael Aguirre Saavedra <michael.aguirre@epchinchorro.cl>
2025-11-19 08:41:51 -0300

    borrador de contrato3

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -109,6 +109,8 @@ export const login = async (req: Request, res: Response) => {
     return;
   }
 
+  //texto de prueba
+
   try {
     const user: any = await Usuario.findOne({ where: { usuario } });
 
